7c5a1c955be12bc69650f0a1db22c0414566230d
brndmst05 <147450384+brndmst05@users.noreply.github.com>
2024-01-07 23:25:49 -0600

    Actualización

---
+++ Cureño-Contreras-Erick-Brandon/practica1/programas_en_C/system/system.c
@@ -0,0 +1,20 @@
+/*
+	Autor: Cureño Contreras Erick Brandon
+	Compilación: gcc -o system system.c
+	Ejecución: ./system
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(){
+    pid_t pid;
+	pid = fork();
+	if(pid == 0){
+		system("Cat system");
+	}
+	else{
+		printf("Soy system\n");
+	}
+	return 0;
+}
